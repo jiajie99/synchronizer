96bf2c774546ccc689a439aef22dc80e62cf8ca0
杰 <985341864@qq.com>
2023-06-15 00:57:58 +0800

    docs: add 202305.md

--- gocn/sync.sh
+++ gocn/sync.sh
@@ -3,4 +3,4 @@ git remote add origin git@github.com:jiajie99/synchronizer.git
 git add .
 git checkout -b "sync-$1"
 git commit -m "docs: add $1.md"
-git push origin
+git push --set-upstream origin "sync-$1"
